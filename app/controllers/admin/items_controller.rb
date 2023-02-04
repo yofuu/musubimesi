f3b968eae484059c89fc5b644ed2e5c9ffb5927c
EC2 Default User <ec2-user@ip-172-31-25-109.ap-northeast-1.compute.internal>
2023-02-04 17:50:18 +0000

    [adminのitem完了]

--- app/controllers/admin/items_controller.rb
+++ app/controllers/admin/items_controller.rb
@@ -1,2 +1,10 @@
 class Admin::ItemsController < ApplicationController
+  def index
+    @items = Item.all
+  end
+  
+  def show
+    @item = Item.find(params[:id])
+  end
+  
 end
--- config/routes.rb
+++ config/routes.rb
@@ -8,5 +8,22 @@ devise_for :customers,skip: [:passwords], controllers: {
 devise_for :admin, skip: [:registrations, :passwords] ,controllers: {
   sessions: "admin/sessions"
 }
+
+# 顧客用
+# root to: 'public/homes#top'
+scope module: :public do
+  resources :items
+  resources :customers
+  get 'customers/unsubscribe'=>'customers#unsubscribe'
+  patch 'customers/withdraw'=>'customers#withdraw'
+end
+
+# 管理者用
+namespace :admin do
+     resources :items
+
+     resources :customers
+     resources :reviews, only: [:destroy]
+   end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
