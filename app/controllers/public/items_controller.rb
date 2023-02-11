1e3f854f16354f8d22e4e029c521b2791ea97bb1
EC2 Default User <ec2-user@ip-172-31-25-109.ap-northeast-1.compute.internal>
2023-02-11 15:27:51 +0000

    [public開始]

--- app/controllers/public/items_controller.rb
+++ app/controllers/public/items_controller.rb
@@ -1,2 +1,25 @@
 class Public::ItemsController < ApplicationController
+  def index
+    @items = Item.all
+  end
+
+  def show
+    @item = Item.find(params[:id])
+  end
+
+  def create
+    @item = Item.new(items_params)
+
+    if @item.save
+      redirect_to item_path(@item.id), notice:"Item was successfully updated."
+    else
+      @items = Item.all
+      render :index
+    end
+  end
+
+  private
+  def items_params
+    params.require(:item).permit(:image, :name, :introduction, :customer_id, :is_active)
+  end
 end
