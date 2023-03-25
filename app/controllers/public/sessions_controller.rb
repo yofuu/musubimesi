400dd07806bec2d023f22c8810c0528b9cf7fddb
EC2 Default User <ec2-user@ip-172-31-25-109.ap-northeast-1.compute.internal>
2023-03-25 09:42:49 +0000

    [実装完了]

--- app/controllers/public/sessions_controller.rb
+++ app/controllers/public/sessions_controller.rb
@@ -2,6 +2,7 @@
 
 class Public::SessionsController < Devise::SessionsController
   # before_action :configure_sign_in_params, only: [:create]
+  before_action :customer_state, only: [:create]
 
   # GET /resource/sign_in
   # def new
